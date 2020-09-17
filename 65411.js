35115e55593dc63fc92bea1eb3a4cf7f7c6eb60f
q-lng <48022083+q-lng@users.noreply.github.com>
2020-09-17 14:20:31 +0200

    Add files via upload

--- 65411.js
+++ 65411.js
@@ -1,15 +1,19 @@
 // ==UserScript==
-// @name        Live-timing VRC ACSM
-// @description Bouton Live-timing VRC ACSM
-// @copyright 2020, 6vx for VRC
-// @license /
-// @version 1.0
-// @match       *://91.121.123.80:*/*
-// @grant       GM_addStyle
-// @require http://code.jquery.com/jquery-3.4.1.min.js
-// @author 6vx
+// @name         Live-timing VRC ACSM
+// @namespace    http://virtual-racers-community.com
+// @version      1.0
+// @description  Bouton Live-timing VRC ACSM
+// @author       6vx
+// @match        *://91.121.123.80:*/*
+// @grant        none
 // ==/UserScript==
 
-$(document).ready(function(){
-$('.navbar-nav.mr-auto').append('<li class="nav-item"><a class="nav-link" href="/live-timing">Live-Timing</a></li>');
-});
+(function() {
+    'use strict';
+
+    $(document).ready(function(){
+    $('.navbar-nav.mr-auto').append('<li class="nav-item"><a class="nav-link" href="/live-timing">Live-Timing</a></li>');
+    });
+
+})();
+
