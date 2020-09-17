87aa0c6bed083a618dcf23f1af748a6550159b8e
q-lng <48022083+q-lng@users.noreply.github.com>
2020-09-17 14:11:31 +0200

    Add files via upload

--- 65411.js
+++ 65411.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name        Live-timing VRC ACSM
 // @description Bouton Live-timing VRC ACSM
+// @copyright 2020, 6vx for VRC
+// @license /
 // @version 1.0
 // @match       *://91.121.123.80:*/*
 // @grant       GM_addStyle
 // @require http://code.jquery.com/jquery-3.4.1.min.js
+// @author 6vx
 // ==/UserScript==
 
 $(document).ready(function(){
